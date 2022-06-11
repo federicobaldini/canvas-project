589086dea4184b35000a7fba5a5a861bbe017363
Federico Baldini <federico.baldini7@gmail.com>
2022-06-11 15:09:35 +0200

    Update canvas.c
    Defined circle struct.

--- libraries/canvas.c
+++ libraries/canvas.c
@@ -1,6 +1,21 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <emscripten.h>
 
+// Number of circles to create and animate
+#define NUM_CIRCLES 10
+
+// Circle Struct
+struct Circle
+{
+  int x;  // x coordinate
+  int y;  // y coordinate
+  int r;  // circle radius
+  int cr; // color - RED
+  int cg; // color - GREEN
+  int cb; // color - BLUE
+};
 // Init circle data and start render - JavaScript
 int main()
 {
