2173e59a1d81d03f980967744fa73ec54a995538
Federico Baldini <federico.baldini7@gmail.com>
2022-06-12 12:35:36 +0200

    Update canvas.c
    Defined the main function, that initialize the circles and the relatives animations, than calls the JavaScript render function..

--- libraries/canvas.c
+++ libraries/canvas.c
@@ -42,8 +42,38 @@ int get_random_number(int min, int max)
 // Init circle data and start render - JavaScript
 int main()
 {
-  printf("Init circles\n");
-  emscripten_run_script("render()");
+  // Seed random number generator
+  srand(time(NULL));
+
+  // Create circles
+  for (int i = 0; i < NUM_CIRCLES; i++)
+  {
+    // Radius
+    int radius = get_random_number(10, 50);
+
+    // Coordinates
+    int x = get_random_number(0, 400) + radius;
+    int y = get_random_number(0, 400) + radius;
+
+    // Fill circle struct - i
+    circles[i].x = x;
+    circles[i].y = y;
+    circles[i].r = radius;
+    circles[i].cr = get_random_number(0, 255);
+    circles[i].cg = get_random_number(0, 255);
+    circles[i].cb = get_random_number(0, 255);
+
+    // Fill animation data struct - i
+    animation_data[i].x = x;
+    animation_data[i].y = y;
+    animation_data[i].r = radius;
+    animation_data[i].xv = get_random_number(1, 4);
+    animation_data[i].yv = get_random_number(1, 4);
+    animation_data[i].xd = 1;
+    animation_data[i].yd = 1;
+  }
+  // Start JavaScript rendering
+  EM_ASM({ render($0, $1); }, NUM_CIRCLES * 6, 6);
 }
 
 // Return circles to JavaScript
